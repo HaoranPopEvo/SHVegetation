ab4f4acd084452fc18e9e72e006358daf0cd26f7
Hao-Ran Wu <106064317+HaoranPopEvo@users.noreply.github.com>
2023-04-08 20:15:15 +0100

    Delete wordcloud.R

--- Figure 1/wordcloud.R
+++
@@ -1,7 +0,0 @@
-require(wordcloud)
-d <- read.csv("Most_Frequent_Words.csv")
-
-colorVec <- rgb(runif(50,0.2,0.8),runif(50,0.2,0.8),runif(50,0.2,0.8))
-wordcloud(d$Terms,d$Frequency,colors = colorVec,scale = c(2.5,0.5))
-
-
